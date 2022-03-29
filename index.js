c127160e1a4ed0b80ae7100e778f0b18ecc684d3
CS Desktop <rylanelarson@gmail.com>
2022-03-29 10:58:54 -0500

    add database connection promise

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import Database from "./src/data/Database.mjs";
 import NikeMonitor from "./src/monitors/NikeMonitor.js";
 import SNKRSMonitor from "./src/monitors/SNKRSMonitor.mjs";
 
-Database.connect();
-console.log('initializing monitor...');
-new NikeMonitor('*/1 * * * *', { sendAlerts: true }).init()
+Database.connect().then(() => {
+    console.log('initializing monitor...');
+    new NikeMonitor('*/2 * * * * *', { sendAlerts: false }).init()
+})
+
 
--- src/data/Database.mjs
+++ src/data/Database.mjs
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 class Database
 {
     static connect() {
-        mongoose.connect(process.env.DB_URL, {
+        return mongoose.connect(process.env.DB_URL, {
             useNewUrlParser: true, useUnifiedTopology: true
         })
             .then(
