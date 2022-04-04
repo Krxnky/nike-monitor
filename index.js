6107b4c8c393cda4c1b9883d0d881dc1fa45dc1f
Krxnky <rylanelarson@gmail.com>
2022-04-03 19:18:41 -0500

    disable debug db

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import NikeMonitor from "./src/monitors/NikeMonitor.js";
 import SNKRSMonitor from "./src/monitors/SNKRSMonitor.mjs";
 import Product from './src/data/schema/Product.js';
 
-Database.connect(true).then(() => {
+Database.connect().then(() => {
     logger.info('initializing monitor...');
     new NikeMonitor('30 */1 * * * *', { sendAlerts: true }).init();
 })
